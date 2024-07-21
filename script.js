51aa819775a95882b68452238672db885ed05dbd
NenadN22 <tonbe22@gmail.com>
2024-07-21 22:02:29 +0200

    Make function so it create divs inside container, also calculate it width and height

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.getElementsByClassName('container')
+function createGrid(squares) {
+    for(let i = 0;  i < squares * squares; i++ ) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        container[0].appendChild(square);
+        console.log(squares)
+        square.style.width = `calc(100% / ${squares})`
+        square.style.height = `calc(100% / ${squares})`
+    }
+}
+createGrid(5);
